43ed69ed88a92a460177f6f2b712201aacdc2336
praveenupadhyay997 <praveenupadhyay997@gmail.com>
2020-11-08 23:51:42 +0530

    [Praveen] Add: Created a DB Model for address book problem.

---
+++ AddressBookProblemLINQ/AddressBookProblemLINQ/Program.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Praveen Kumar Upadhyay"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace AddressBookProblemLINQ
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("========================================================");
+            Console.WriteLine("Welcome to the Address Book Data LINQ Operartion Program");
+            Console.WriteLine("========================================================");
+        }
+    }
+}
